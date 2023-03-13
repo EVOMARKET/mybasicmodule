35d5a2c7a398c15528186cd4b30cd5bfe4a6631f
EVOMARKET <biuro@evomarket.pl>
2023-03-13 19:38:35 +0100

    and the lesson 41

--- src/controller/CommentController.php
+++ src/controller/CommentController.php
@@ -1,16 +1,22 @@
 <?php 
 namespace  Mybasicmodule\Controller;
-use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
 use Symfony\Component\HttpFoundation\Response;
-class CommentController extends FrameworkBundleAdminController
+use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
+
+class CommentController extends FrameworkBundleAdminController
 {
     public function indexAction()
     {
+        $form = $this ->createFormBuilder()
+        ->add('name' , TextType::class)
+        ->getForm();
       // return new Response ("Hello world");
       return $this ->render("@Modules/mybasicmodule/views/templates/admin/comment.html.twig",
       [
-        'test'=> 123
+        "test"=> 123,
+        "form"=>$form->createView()
       ]
     );
       
