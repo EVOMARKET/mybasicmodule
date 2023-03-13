99b87857eedf7925077096c24c041b61db6794cf
EVOMARKET <biuro@evomarket.pl>
2023-03-13 18:15:47 +0100

    and lesson 37

---
+++ src/controller/CommentController.php
@@ -0,0 +1,10 @@
+<?php 
+namespace  Mybasicmodule\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+class CommentController
+{
+    public function indexAction(){
+       return new Response ("Hello world");
+    }
+}
